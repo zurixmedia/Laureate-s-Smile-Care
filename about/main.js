0cb8b36cad26d60a0edc46b5cc792ceeb20d1c46
zurixmedia <bryanigberaese@gmail.com>
2024-11-29 16:40:03 -0700

    Add files via upload

---
+++ about/main.js
@@ -0,0 +1,43 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open__nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+openNavBtn.addEventListener('click', () => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block'
+})
+
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none'
+}
+
+closeNavBtn.addEventListener('click', closeNav)
+
+
+
+// TESTIMONIALS SECTION (swiper js)
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Responsive Breakpoints
+    breakpoints: {
+        // when window width is >= 600px
+        600: {
+            slidesPerView: 2
+        },
+
+        // when window width is >= 600px
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
